57a2832b5c97689916322a5a8cde2dfd7886b156
therookie636 <therookie636@gmail.com>
2021-04-21 13:52:27 +0530

    Register page started

--- djangoProject/mainapp/urls.py
+++ djangoProject/mainapp/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('home', views.home),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register")
 ]
--- djangoProject/mainapp/views.py
+++ djangoProject/mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
+from .forms import createUserForm
 
 # Create your views here.
 def index(request):
@@ -7,4 +10,17 @@ def index(request):
 
 def home(request):
     return HttpResponse('<h1>Hello there</h1>')
+
+def login(request):
+    return render(request, 'Accounts/login.html')
+
+@csrf_exempt
+def register(request):
+    form = createUserForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+    context={"form": form}
+    return render(request, 'Accounts/register.html', context)
 
