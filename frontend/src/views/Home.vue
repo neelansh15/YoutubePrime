bc825fd8aac9b9f7630a64c69f829e954fb55132
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-04-26 22:07:28 +0530

    Minor homepage edits

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <v-container fill-height>
     <v-row justify="center" align="center">
-      <v-col class="text-h1">
-        <v-icon class="text-h1"> mdi-youtube </v-icon>
-        YouTube Prime
-
-        <div >
-
+      <v-col class="text-h5">
+        <div style="display: flex; align-items: center">
+          <v-icon class="text-h4"> mdi-youtube </v-icon>
+          <span class="ml-1">YouTube Prime</span>
+        </div>
+        <div>
+          <h3 class="text-h1">
+            The next generation of subscription based video sharing
+          </h3>
+          <v-btn>Login</v-btn>
         </div>
       </v-col>
     </v-row>
