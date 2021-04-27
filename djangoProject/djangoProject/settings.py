c84b08cf838526ce7f58bc274df238f788919987
pss10 <parthsatyen@gmail.com>
2021-04-27 11:17:06 +0400

    Video-Upload

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp'
+    'mainapp',
+    'video'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
---
+++ djangoProject/video/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Video
+admin.site.register(Video)
