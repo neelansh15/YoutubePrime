5c9605816062d99aae1a729f5e92b7eae13e8e50
VedantMahadik <therookie636@gmail.com>
2021-04-27 22:48:30 +0530

    Added two pages login and register

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,47 +3,60 @@ import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Home from '../views/Home.vue'
 import Play from '../views/Play.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 import PlayOwner from '../views/PlayOwner.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
-  },
-  {
-    path: '/play',
-    name: 'Play',
-    component: PlayOwner,
-    children: [
-      {
-        path: '/play/:id',
-        name: 'Play',
-        component: Play
-      }
-    ]
-  },
-  {
-    path: '/account',
-    name: 'Account',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Account.vue')
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: Dashboard,
+	},
+	{
+		path: '/play',
+		name: 'Play',
+		component: PlayOwner,
+		children: [
+			{
+				path: '/play/:id',
+				name: 'Play',
+				component: Play,
+			},
+		],
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login,
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: Register,
+	},
+	{
+		path: '/account',
+		name: 'Account',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/Account.vue'),
+	},
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
 })
 
 export default router
