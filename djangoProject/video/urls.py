5f0c6899c8a357bdfbb52ad57e70a209bc63eedf
VedantMahadik <therookie636@gmail.com>
2021-04-28 22:42:16 +0530

    Minor changes

--- djangoProject/video/urls.py
+++ djangoProject/video/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index)
+    # path('',views.index)
     # path('',views.index)
     path('video/',VideoView.as_view(),name='video_view')
 
