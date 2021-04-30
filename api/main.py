57e737751feaa28c07871db9a316c48a4757ffb2
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-04-30 22:03:27 +0530

    Updated flask

--- api/main.py
+++ api/main.py
@@ -1,6 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
 @app.route("/")
 def hello():
     return "Hello world!"
+
+@app.route("/register")
+def register():
+    user = auth.create_user(
+        email='user@example.com',
+        email_verified=False,
+        phone_number='+15555550100',
+        password='secretPassword',
+        display_name='John Doe',
+        photo_url='http://www.example.com/12345678/photo.png',
+        disabled=False
+    )
+    print('Sucessfully created new user: {0}'.format(user.uid))
