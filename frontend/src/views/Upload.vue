2ac303bff563d28f46e36b2f5745c03fbb9f0d38
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-05-07 15:08:41 +0530

    Minor route fix + upload video FormData

--- frontend/src/views/Upload.vue
+++ frontend/src/views/Upload.vue
@@ -1,36 +1,73 @@
 <template>
-	<v-container class="mt-4">
-		<h1>Upload a new video</h1>
+  <v-container class="mt-4">
+    <h1>Upload a new video</h1>
 
-		<!-- Dropzone element -->
-		<!-- <dropzone ref="dropzone" :options="dropzoneOptions" duplicateCheck /> -->
-		<v-form class="mt-5" ref="form" @submit.prevent="">
-			<v-file-input></v-file-input>
-			<v-text-field :counter="50" label="Title"></v-text-field>
-			<v-textarea label="Description" :counter="200"></v-textarea>
-			<v-btn class="primary">Confirm</v-btn>
-			<v-btn class="secondary">Cancel</v-btn>
-		</v-form>
-	</v-container>
+    <v-form class="mt-5" ref="form" @submit.prevent="upload">
+      Video:
+      <v-file-input
+        name="myfile"
+        v-model="myfile"
+        accept="video/*"
+		show-size
+      ></v-file-input>
+      Thumbnail:
+      <v-file-input
+        name="thumbnail"
+        v-model="thumbnail"
+        accept="image/*"
+		show-size
+      ></v-file-input>
+      <v-text-field :counter="50" label="Title" v-model="title"></v-text-field>
+      <v-textarea
+        label="Description"
+        :counter="200"
+        v-model="description"
+      ></v-textarea>
+      <v-btn class="primary">Upload</v-btn>
+      <!-- <v-btn class="secondary">Cancel</v-btn> -->
+    </v-form>
+  </v-container>
 </template>
 
 <script>
-// import dropzone from "vue2-dropzone";
+// import axios from "axios";
 
 export default {
-	name: 'Upload',
-	components: {
-		// dropzone,
-	},
-	data: () => ({
-		dropzoneOptions: {
-			url: 'https://httpbin.org/post',
-			thumbnailWidth: 150,
-			maxFilesize: 0.5,
-			headers: { 'My-Awesome-Header': 'header value' },
-		},
-	}),
-}
+  name: "Upload",
+  components: {
+    // dropzone,
+  },
+  data: () => ({
+    myfile: null,
+    thumbnail: null,
+    title: "",
+    description: "",
+  }),
+  methods: {
+    upload(e) {
+      console.log(e);
+
+      let formData = new FormData();
+      formData.append("myfile", this.myfile);
+      formData.append("thumbnail", this.thumbnail);
+      formData.append("title", this.title);
+      formData.append("description", this.description);
+	console.log(formData)
+    //   axios.post(this.$store.state.baseURL + "/upload", formData, {
+    //     headers: {
+    //       "Content-Type": "multipart/form-data",
+    //     },
+    //     onUploadProgress: (e) => {
+    //       console.log(e);
+    //     },
+    //   });
+    },
+    onUploadProgress(e) {
+      console.log("Upload progress: ");
+      console.log(e);
+    },
+  },
+};
 </script>
 
 <style></style>
